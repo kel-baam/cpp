1cb3d8885b27fdc9f1f6cc4f554cf3b9b3fc3387
Kaoutar El Baamrani <kel-baam@e1r11p8.1337.ma>
2023-08-09 16:03:09 +0100

    solve ex05

--- day01/ex05/Harl.cpp
+++ day01/ex05/Harl.cpp
@@ -1,16 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kel-baam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 15:58:11 by kel-baam          #+#    #+#             */
+/*   Updated: 2023/08/09 15:58:17 by kel-baam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "Harl.hpp"
 
+void Harl::debug( void )
+{
+    std::cout<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"<< std::endl;
+}
 
+void Harl::info( void )
+{
+    std::cout<<"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"<<std::endl;
+}
 
+void Harl::warning( void )
+{
+    std::cout<<"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."<<std::endl;
+}
 
+ void Harl::error( void )
+ {
+    std::cout<<"This is unacceptable! I want to speak to the manager now."<<std::endl;
+ }
+ 
 void Harl::complain( std::string level)
 {
-    if(level == "DEBUG" )
-        void debug( void );
-    if(level == "INFO")
-        void info( void );
-    if(level == "WARNING" )
-        void warning( void );
-    if(level == "ERROR")
-        void error( void );
+    void (Harl::*debugPtr[])()  = {&Harl::debug, &Harl::info,&Harl::warning, &Harl::error};
+     if(level == "DEBUG")
+         (this->*debugPtr[0])();
+    else if(level == "INFO")
+       (this->*debugPtr[1])();
+    else if(level == "WARNING")
+       (this->*debugPtr[2])();
+    else if(level == "ERROR")
+        (this->*debugPtr[3])();
+    else
+        std::cout<<"Invalid level"<< std::endl;
 }
--- day01/ex05/Harl.hpp
+++ day01/ex05/Harl.hpp
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kel-baam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 15:58:22 by kel-baam          #+#    #+#             */
+/*   Updated: 2023/08/09 15:58:24 by kel-baam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <iostream>
 
 class Harl
 {
     public:
+      
       void complain( std::string level ); 
     private:
+        void hello();
         void debug( void );
         void info( void );
         void warning( void );
